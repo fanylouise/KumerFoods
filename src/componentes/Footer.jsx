3a971fbf244956b13f1f2d3037a3265fd24a4586
fanylouise <gandra.fany@gmail.com>
2022-09-12 12:24:37 -0300

    :construction: | Navigation

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -11,7 +11,8 @@ const Footer = ()=>{
         <li><a href="https://beacons.ai/fanylouise" target='_blank'>kumer@gmail.com</a> </li>
         <li><a href="https://beacons.ai/fanylouise" target='_blank'>(00)987654321</a></li>
       </ul>
-      <p className='rights'>Todos os  direitos reservados <Logo_Kumerfoods/> 2022</p>
+      <p className='rights'>Imagens meramente ilustrativas.
+      Todos os  direitos reservados <Logo_Kumerfoods/> 2022</p>
     </>
   )
 }
