23efb8dfa464ba7f289fc212888642ab499a3cf8
fanylouise <gandra.fany@gmail.com>
2023-01-03 19:02:41 -0300

    Less distance in contacts | new display of cards

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,6 @@ import Footer from './componentes/Footer/Footer';
 
 import Title from './componentes/Title/Title';
 
-import Logo_Kumerfoods from './componentes/Logo/Logo-KumerFoods';
-
-{/*Ilustrations*/}
-
 function App(){
 
   return (
@@ -22,9 +18,9 @@ function App(){
       </header>
       <main className='mainTop'>
         <div className='top'>
-        <section className='options'>
-          <Title text='Opções'/>
-         <div className='cubeCardsFomation'>
+        <section className='Options'>
+            <Title text='Opções'/>
+            <div className='cubeCardsFomation'>
             <Card />
             <Card />
             <Card />
@@ -33,30 +29,27 @@ function App(){
         </section>
         {/*Famous*/}
         <section className='Famous'>
-         <Title text='Famosos' />
-          <div className='cubeCardsFomation'>
+            <Title text='Famosos' />
+            <div className='cubeCardsFomation'>
             <Card />
             <Card />
             <Card />
             <Card />
-         </div>
+            </div>
         </section>
-
-        </div>
-
+      </div>
 
        {/*New*/}
-        <div className='bottom'>
+        <section className='bottom'>
           <Title text='Novos' />
-          <section className='New'>
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-           </section>
-        </div>
+            <div className='New'>
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+           </div>
+        </section>
 
-       
       </main>
 
 
